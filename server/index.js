b975f4faddd68d428e9d74e72030797691285426
Demo <kartikkumbhar1812@gmail.com>
2025-12-28 20:05:26 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 import route from "./routes/userRoutes.js";
 import tranRoutes from "./routes/transactionRoutes.js";
 import dotenv from "dotenv";
@@ -13,22 +13,24 @@ import serverless from "serverless-http";
 
 const app = express();
 
-const corsOption ={
-        origin:"http://localhost:5173",
-        methods:"GET,POST,PUT,DELETE",
-        credentials:true
+const corsOption = {
+        origin: ["http://localhost:5173",
+                "https://rupayamate.vercel.app"
+        ],
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true
 }
 
 app.use(cors(corsOption));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-        .then(()=>console.log("DB Connected!!!"))
-        .catch((error)=>console.log(error));
+        .then(() => console.log("DB Connected!!!"))
+        .catch((error) => console.log(error));
 
-app.use("/api",route);
-app.use("/api/transaction",tranRoutes);
+app.use("/api", route);
+app.use("/api/transaction", tranRoutes);
 
 app.use(errorMiddleware);
 
