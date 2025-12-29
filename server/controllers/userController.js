6fef6b0c1897e2c23c89b1568ca8eccd706ac457
Demo <kartikkumbhar1812@gmail.com>
2025-12-29 14:34:23 +0530

    token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,8 +27,9 @@ export const registerUser = async (req, res) => {
         }
         );
 
+        const token =jwt.sign({id:newUser._id,name:newUser.name},process.env.JWT_SECRET,{expiresIn:"1d"});
 
-        return res.status(200).json({ message: "User created Successfully" });
+        return res.status(200).json({ message: "User created Successfully" ,token});
     }
     catch (error) {
         return res.status(500).json({ errormessage: error.message });
