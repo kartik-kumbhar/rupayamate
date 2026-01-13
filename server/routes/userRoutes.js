68bb2a895f98ae070388f9ff63538d9426ce0f87
Demo <kartikkumbhar1812@gmail.com>
2026-01-13 09:47:20 +0530

    gitignore

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import {  loginUser, registerUser ,getCurrent} from "../controllers/userController.js";
+import { loginUser, registerUser, getCurrent } from "../controllers/userController.js";
 import { loginSchema, signupSchema } from "../validation/auth-validator.js";
 import { validate } from "../middleware/validate.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const route = express.Router();
 
-route.post("/users/register",validate(signupSchema) ,registerUser);
-route.post("/users/login",validate(loginSchema),loginUser);
-route.get("/users/user",protect,getCurrent);
+route.post("/users/register", validate(signupSchema), registerUser);
+route.post("/users/login", validate(loginSchema), loginUser);
+route.get("/users/user", protect, getCurrent);
 
 export default route;
+
+
+
